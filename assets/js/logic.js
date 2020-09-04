d1ef56718016eff4c6d76f8c388a57498ccbcbd5
NikolasKotsikos <kotsikn@gmail.com>
2020-09-04 16:38:23 +0000

    Work on logic.js and index.html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,31 +1,56 @@
-//Global variables
-var playerIcon; 
-var victoryConditions;
-var win; //True if a player wins the game
-var turn; //Defines consecutive turns 
+const p1Turn = 'Player 1'
+const p2Turn = 'Player 2'
+const victory_conditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const boxElements = document.querySelectorAll('[data-box]')
+const board = document.getElementById('board')
+const infoElement = document.getElementById('info')
+const resetButton = document.getElementById('reset')
+const scoreTextElement = document.getElementById('score')
+let nextTurn
 
-$(document).ready(function() {
-  // Faction Selector
-  $(".aa").on("click", function() {
-    playerIcon = "assets/images/aalogo.png";
-    }  
-  });
+//player selects faction
+$('.nav-item').on('click mouseover mouseout', function(e) {
+    this.addClass('active');
+    
+}
 
-  $(".cha").on("click", function() {
-    playerIcon = "assets/images/chaoslogo.png";
-    }  
-  });
 
-  $(".eld").on("click", function() {
-    playerIcon = "assets/images/eldarlogo.png";
-      
-  });
+startGame()
 
-  $(".nec").on("click", function() {
-    playerIcon = "assets/images/necronslogo.png";
-      
-  });
 
-  $(".tau").on("click", function() {
-    playerIcon = "assets/images/taulogo.png";
- });
+
+
+//player clicks on a box 
+$('[data-box]').on('click', function(e) {
+  const box = e.target
+  const currentTurn = nextTurn ? p2Turn : p1Turn
+  placeIcon(box, currentTurn)
+  if (checkWin(currentTurn)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    changeTurn()
+}
+}
+
+//place player icon
+function placeIcon(box, currentTurn) {
+  box.classList.add(currentTurn)
+}
+
+//change turn
+function changeTurn() {
+  nextTurn = !nextTurn
+}
+//check for empty boxes
+//check for victory conditions
