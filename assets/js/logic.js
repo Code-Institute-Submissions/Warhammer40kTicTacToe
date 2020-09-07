6d1362c2554d38ac3df7caf784dd24242f41e648
NikolasKotsikos <kotsikn@gmail.com>
2020-09-07 10:57:47 +0000

    Work on logic.js and minor cleanup in index.html code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,56 +1,38 @@
-const p1Turn = 'Player 1'
-const p2Turn = 'Player 2'
-const victory_conditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
-const boxElements = document.querySelectorAll('[data-box]')
-const board = document.getElementById('board')
-const infoElement = document.getElementById('info')
-const resetButton = document.getElementById('reset')
-const scoreTextElement = document.getElementById('score')
-let nextTurn
+document.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelectorAll("#board .box")
+  const selectBtn = document.querySelector('.select')
+  const turnDisplay = document.querySelector("#info")
 
-//player selects faction
-$('.nav-item').on('click mouseover mouseout', function(e) {
-    this.addClass('active');
-    
-}
+  let currentPlayer = "Player 1"
+  
+  selectBtn.forEach(button) => {
+      selectBtn.addEventListener("click", selectResult)
+  }
 
+  function selectResult(e) {
+      const selectArray = Array.from(selectBtn)
+      const selectIndex = selectArray.indexOf(e.target)
 
-startGame()
+      if() {
 
+      }
+  }
 
+  box.forEach((box) => {
+  box.addEventListener("click", clickResult)
+})
 
+function clickResult(e) {
+  const boxArray = Array.from(box);
+  const index = boxArray.indexOf(e.target)
+  turnDisplay.innerHTML = currentPlayer + "Plays Next"
 
-//player clicks on a box 
-$('[data-box]').on('click', function(e) {
-  const box = e.target
-  const currentTurn = nextTurn ? p2Turn : p1Turn
-  placeIcon(box, currentTurn)
-  if (checkWin(currentTurn)) {
-    endGame(false)
-  } else if (isDraw()) {
-    endGame(true)
+  if(currentPlayer === "Player 1") {
+      box[index].classList.add("Player1")
+      currentPlayer = "Player 2"
   } else {
-    changeTurn()
+      box[index].classList.add("Player2")
+      currentPlayer = "Player 1"
+  }
 }
 }
-
-//place player icon
-function placeIcon(box, currentTurn) {
-  box.classList.add(currentTurn)
-}
-
-//change turn
-function changeTurn() {
-  nextTurn = !nextTurn
-}
-//check for empty boxes
-//check for victory conditions
