a6571aa2b049b00c7d2b52ad45aed890f0694f5c
NikolasKotsikos <kotsikn@gmail.com>
2020-09-09 10:41:00 +0000

    Refactored code with jquery,added turn validation and new game functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,72 +1,112 @@
-let playerIcon = $();
-let cpuIcon = $();
-const box = document.querySelectorAll("#board .box");
-const turnDisplay = document.querySelector("#info");
-let turnCount = 0;
 
-let currentPlayer = "Player 1";
+$(document).ready(function() {
+
+var playerIcon = $();
+var cpuIcon = $();
+var turnCount = 0;
+var turnValid = false;
 
 $("#aa").on("click", function () {
   $("li .select", !this).css("background-color", "#fafafa");
   $(this).css("background-color", "blue").attr("aria-pressed", true);
   $("body").css("background-color", "blue");
-  $(".newgame").css("background-color", "green");
-  playerIcon.addClass(".aa");
+  $(".newgame").addClass("ready");
+   $(".newgame").css("background-color", "green");
+   $(".newgame:hover").css("background-color", "green");
+  playerIcon.addClass("aa")
 });
 
 $("#cha").on("click", function () {
   $("li .select", !this).css("background-color", "#fafafa");
   $(this).css("background-color", "red").attr("aria-pressed", true);
   $("body").css("background-color", "red");
-  $(".newgame").css("background-color", "green");
-  playerIcon.addClass(".cha");
+  $(".newgame").addClass("ready");
+   $(".newgame").css("background-color", "green");
+   $(".newgame:hover").css("background-color", "green");
+  playerIcon.addClass("cha");  
 });
 
 $("#eld").on("click", function () {
   $("li .select", !this).css("background-color", "#fafafa");
   $(this).css("background-color", "green").attr("aria-pressed", true);
   $("body").css("background-color", "green");
+  $(".newgame").addClass("ready");
   $(".newgame").css("background-color", "green");
-  playerIcon.addClass(".eld");
+  $(".newgame:hover").css("background-color", "green");
+  playerIcon.addClass("eld");
 });
 
 $("#nec").on("click", function () {
   $("li .select", !this).css("background-color", "#fafafa");
   $(this).css("background-color", "silver").attr("aria-pressed", true);
   $("body").css("background-color", "silver");
-  $(".newgame").css("background-color", "green");
-  playerIcon.addClass(".nec");
+  $(".newgame").addClass("ready");
+   $(".newgame").css("background-color", "green");
+   $(".newgame:hover").css("background-color", "green");
+  playerIcon.addClass("nec");
 });
 
 $("#tau").on("click", function () {
   $("li .select", !this).css("background-color", "#fafafa");
   $(this).css("background-color", "orange").attr("aria-pressed", true);
   $("body").css("background-color", "orange");
-  $(".newgame").css("background-color", "green");
-  playerIcon.addClass(".tau");
+  $(".newgame").addClass("ready");
+   $(".newgame").css("background-color", "green");
+   $(".newgame:hover").css("background-color", "green");
+  playerIcon.addClass("tau");
 });
 
-$(".ng").on("click", startGame);
-
-function startGame() {
-  $("#board input").removeClass(".playerIcon", ".cpuIcon");
-}
-
-box.forEach((box) => {
-  box.addEventListener("click", clickResult);
+$(".newgame").on("click", function () {
+  $(".box").removeClass("aa");
+  $(".box").removeClass("cha");
+  $(".box").removeClass("eld");
+  $(".box").removeClass("nec");
+  $(".box").removeClass("tau");
+  $(".box").addClass("free");
+  $("#info").text("Player 1 Plays")
+  turnCount = turnCount + 1;
 });
 
-function clickResult(e) {
-  const boxArray = Array.from(box);
-  const index = boxArray.indexOf(e.target);
-  turnDisplay.innerHTML = currentPlayer + " Plays Next";
-  turnCount = turnCount + 1 < 9;
+function validateTurn(boxplayed) {
+	if ( $(boxplayed).hasClass('free') ) {
+		turnValid = true;
+	} else {
+		turnValid = false;
+		return false;
+	}
+};
+  
+$(".box").on("click", function () { 
+    if(turnCount == 1, $(".newgame").hasClass("ready")) { 
+    $("#info").text("Player 2 Plays Next"); 
+    $(this).prepend(playerIcon);  
+    turnCount = 2;             
+    } else {     
+        $("#info").text("Player 1 Plays Next"); 
+        $(this).prepend(cpuIcon);  
+        turnCount = 1; 
+    } 
+}); 
 
-  if (currentPlayer === "Player 1") {
-    box[index].innerHTML = playerIcon;
-    currentPlayer = "Player 2";
-  } else {
-    box[index].innerHTML = cpuIcon;
-    currentPlayer = "Player 1";
-  }
-}
+$(".box").on('click', function player1Turn() {
+
+	validateTurn(this);
+
+	if (turnValid, $(".newgame").hasClass("ready")) {
+		$(this).removeClass("free");
+		$(this).addClass("played");
+		$(this).addClass("player1Move");
+		$(this).html(playerIcon);
+
+		/*checkDraw();
+		checkWin();
+		cpuTurn();*/
+
+	} else {
+		$(this).css({"background-color": "red",
+                     "opacity": "0.2"});
+	};
+	
+})
+
+})
