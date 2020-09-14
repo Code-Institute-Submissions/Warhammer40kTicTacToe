72cc8f04af5b8ad82d5a0168446769b53e15a512
NikolasKotsikos <kotsikn@gmail.com>
2020-09-14 16:02:47 +0000

    Further work on logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,13 @@
-
-$(document).ready(function() {
-
+$(document).ready(function () {
   let playerIcon;
   let cpuIcon;
-  let turnCount;
   var turnValid = false;
+  var startButton = $(".newgame");
   var box = document.querySelectorAll(".box");
   var selector = document.querySelectorAll(".select");
-  
+
   $(selector).on("click", function (e) {
-    $(`.select:not(#${e.target.id})`).attr("disabled", true).css({"cursor": "not-allowed", "background-color": "lightblue"});
-    let startButton = $(".newgame")
+    $(`.select:not(#${e.target.id})`).attr("disabled", true).css({"cursor": "not-allowed", "background-color": "lightgray"});
     startButton.addClass("ready");
     startButton.css("background-color", "green");
     $(".newgame:hover").css("background-color", "green");
@@ -19,92 +16,114 @@ $(document).ready(function() {
 
     $(this).css("background-color", factionColor).attr("aria-pressed", true);
     $("body").css("background-color", factionColor);
-    playerIcon = `<img src="assets/images/${e.target.id}logo.png" class ="playerIcon ${e.target.id}" alt="${e.target.id}_logo" />`;
+    playerIcon = `<img src="assets/images/${e.target.id}logo.png" class ="${e.target.id}" alt="${e.target.id}_logo" />`;
 
-    cpuIconSelect(!this);
+    cpuIconSelect();
   });
 
-function cpuIconSelect() {
-    var randomNumber = Math.floor((Math.random()*))
-}
+  function cpuIconSelect() {
+    let iconArray = [
+      `<img src="assets/images/aalogo.png" class ="aa" alt="aa_logo" />`,
+      `<img src="assets/images/chalogo.png" class ="cha" alt="cha_logo" />`,
+      `<img src="assets/images/eldlogo.png" class ="eld" alt="eld_logo" />`,
+      `<img src="assets/images/neclogo.png" class ="nec" alt="nec_logo" />`,
+      `<img src="assets/images/taulogo.png" class ="tau" alt="tau_logo" />`
+    ];
+    let arrayLength = iconArray.pop(playerIcon);
 
-$(".newgame").on("click", function (e) {
- if  ($(e.target).hasClass("ready"), turnCount) {
-  $(".box").remove("img");
-  $(".box").removeClass("played");
-  $(".box").addClass("free");
-  $(".box").css("cursor", "pointer");
-  $("#info").text("Player 1 Plays");
-  turnCount = 1;
- } else {
-     return false;
- }
-    });
+    randomSelect = Math.floor(Math.random() * arrayLength);
+    cpuIcon = randomSelect;
+    return cpuIcon;
+  };
 
-function validateTurn(box) {
-	if ( $(box).hasClass('free'), $(box).empty()) {
-        turnValid = true;
-        
-	} else {
-		turnValid = false;
-		return false;
-	}
+  $(startButton).on("click", function () {
+    if ($(this).hasClass("ready")) {
+      $(box).remove("img");
+      $(box).addClass("free");
+      $(box).css("cursor", "pointer");
+      $(this).attr("disabled", true).css("cursor", "not-allowed");
+      $("#info").text("Player Turn");
+
+      player1Turn();
+    } else {
+      return false;
+    }
+  });
+
+  function validateTurn(box) {
+    if ($(box).hasClass("free")) {
+      turnValid = true;
+    } else {
+      turnValid = false;
+      return false;
+    }
+  }
+
+  function player1Turn() {
+      $(box).on("click", function (e) {
+
+    validateTurn(e.target);
+
+    if (turnValid === true, $(startButton).hasClass("ready")) {
+      $(this).removeClass("free");
+      $(this).addClass("playerMove");
+      $(this).css("padding", "0");
+      $(this).prepend(playerIcon);
+      $("#info").text("Cpu Turn Next");
+      cpuTurn();
+      //checkDraw();
+	  //checkWin();
+    } else {
+      $(this).css({ "background-color": "red", opacity: "0.2" });
+      $(this).fadeOut(800);
+      $("#info").text("Invalid Move");
+    }
+
+    $("#restart", "#restart:hover").css("background-color", "red").attr("disabled", false);
+  });
 };
-  
-$(box).on("click", function () { 
-    if(turnCount === 1, $(".newgame").hasClass("ready")) { 
-    $("#info").text("Player 2 Plays Next"); 
-    turnCount = 2;             
-    } else {     
-        $("#info").text("Player 1 Plays Next"); 
-        turnCount = 1; 
-    } 
-}); 
 
-$(box).on('click', function player1Turn(e) {
-   
-	validateTurn(this);
+  function cpuTurn() {
+    
+    for (i = 0; i < 9; i++) {
+    
+    let randomNumber = Math.floor((Math.random() * 9) + 1);
+    let boxId = $(box.hasId(Number));
+    var randomBox = boxId.eq(randomNumber);
 
-	if (turnValid, $(".newgame").hasClass("ready"),(this).hasClass("free")) {
-		$(this).removeClass("free");
-		$(this).addClass("played");
-        $(this).addClass("player1Move");
-        $(this).css("padding", "0");
-        $(this).prepend(playerIcon);
-       turnCount++
-		/*checkDraw();
-		checkWin();
-		cpuTurn();*/
+    validateTurn(randomBox);
 
-	} else {
-		$(this).css({"background-color": "red",
-                     "opacity": "0.2"});
-	};
-    $("#restart", "#restart:hover").css("backgrond-color", "red").attr("aria-pressed", false);
-    setTimeout(() => { 
-            $(this).css("background-color", "white"); 
-        }, 800);         
-})
+     if (turnValid === true) {
+      randomBox.removeClass("free");
+      randomBox.addClass("cpuMove");
+      randomBox.css("padding", "0");
+      randomBox.prepend(cpuIcon);
+      $("#info").text("Player Turn Next");
 
+       //checkDraw();
+	  //checkWin();
+      player1Turn()
+    } else {
+      return false;
+  };
+};
+};
 
-function restart() {
-	$(box).removeClass("played");
-	$(box).removeClass("Player2Move");
-	$(box).removeClass("Player1Move");
-	$(box).remove("img");
+  function restart() {
+    $(box).removeClass("cpuMove");
+    $(box).removeClass("playerMove");
+    $(box).remove("img");
     $(box).addClass("free");
-    $(selector).remove("background-color", factionColor).attr("aria-pressed", false);
+    $(selector).remove("background-color", factionColor).ramoveAttr("disabled");
     $("body").remove("background-color", factionColor);
-    turnCount = 0; 
-};
+    $("#info").text("Select A Faction And Press Start A New Game");
+   }
 
-$("#restart").on("click", function() {
+  $("#restart").on("click", function () {
     restart();
-    $(this).remove("background-color");
-});
+    $(this).attr("disabled", true);
+  });
 
-/*function checkDraw(){}
-function checkWin(){}
-function cpuTurn(){}*/
-
+//function checkDraw(){}
+//function checkWin(){}
 });
